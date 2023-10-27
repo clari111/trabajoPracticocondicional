c0a840928bccb3eb545eff95070b2e48ddd0128a
alumno <alumno@Instituto.local>
2023-10-27 09:29:47 -0300

    27 de octubre

---
+++ condicional.js
@@ -0,0 +1,48 @@
+/*2*/
+if (edad >= 18)
+{ alert ('sos mayor de edad ')
+}else{
+    alert ('sos menor de edad')
+}
+
+/*1*/
+if ((nombre == 'Santino') || (nombre == 'Nahuel'))
+{
+    alert ('Bienvenido, desaprobado')
+} else{
+    alert ('Bienvenido alumn@')
+}
+
+/*3*/
+
+if ((edadPersona >=6) && ( edadPersona <= 11))
+{
+    alert ('Sos un niño')
+} else if 
+    ((edadPersona >=12) && ( edadPersona <= 18))
+    {
+        alert ('Sos adolescente')
+    } else if
+    ((edadPersona >=19) && ( edadPersona <= 26))
+    {
+        alert ('sos un joven')
+    } else if
+    ((edadPersona >= 27) && ( edadPersona <= 59))
+    {
+        alert ('Sos una adulto')
+    } else if
+
+   (edadPersona >=60) 
+   {
+       alert ('Eres un anciano')
+   }
+
+   /*4*/
+
+
+if ((diaDelaSemana = ('Lunes')) || ((diaDelaSemana = ('Martes')) || ((diaDelaSemana = ('Miercoles')) || ((diaDelaSemana = ('Jueves')) || ((diaDelaSemana = ('Viernes')) 
+{
+alert ('Es dia laboral')
+} else{
+    
+}
